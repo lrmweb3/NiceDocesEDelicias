ce764b70737fb20f495ce1bb372d5603c9edf2ee
MovimentoWeb <reginaldo963@gmail.com>
2015-05-12 14:35:20 -0300

    Arquivos base do site Nise doces e delícias

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" >
+<html lang="pt-BR">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Nice doces e Delícias</title>
+        <link rel="shortcut icon" href="img/logo.ico" />
+    </head>
+    <body>
+        <div id="body">
+            <div id="container">Nice</div>
+        </div>
+    </body>
+</html>
+
