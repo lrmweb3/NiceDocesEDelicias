2a9e6fe724fe6df6d31092365e9853d57c3e3ecb
MovimentoWeb <reginaldo963@gmail.com>
2015-05-20 12:43:57 -0300

    Criação da página de Produtos Itens

---
+++ application/controllers/produtos.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+class Produtos extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('sistema');
+        init_site();
+    }
+
+    public function index() {
+
+        set_tema('CSSpagina', '', FALSE);
+        set_tema('JSpagina', '', FALSE);
+
+
+        set_tema('header', load_modulo('header', '', 'site/includes'));
+        set_tema('paginas', load_modulo('produtos_view', '', 'site/pg/produtos'));
+        set_tema('footer', load_modulo('footer', '', 'site/includes'));
+
+        load_template();
+    }
+
+}
