2993d959092c0aee364e0e95c982f3d3892fa64a
MovimentoWeb <reginaldo963@gmail.com>
2015-05-20 12:18:22 -0300

    Página pesquisar OK - Alterada!

--- application/views/site/pg/pesquisar_view.php
+++ application/views/site/pg/pesquisar_view.php
@@ -1,178 +1,134 @@
-<div class="container pesquisar">
-    
-    <div id="gradiente_pesquisa">
-        <div id="texto_resultado">
-        <font id="fonte1">Foram encontrados <font id="fonte2">90</font> resultados</font>
-        </div>
+
+<div class="gradiente_pesquisa img-rounded">
+    <div class="texto_resultado">
+        <font>Foram encontrados <strong>90</strong> resultados</font>
     </div>
-    
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+</div>
+<div class="container pesquisar">
+
+    <div class="result-group">
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input id="btn_detalhes" type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input id="btn_compartilha" type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-    <div id="resultado_pesquisa" >
-        <img class="img img-polaroid" data-src="holder.js/300x200/auto"/>
-        
-        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
-            conteudo referente a imagem
-            mostrada acima. Especificações do produto
-            em questão.
-            
-            <div id="btn_pesquisa">
-                <input type="button" class="btn-warning"  value="Ver detalhe [+]"/>
-                <input type="button" class="btn-success" value="Indicar para um amigo >>" />
+        <div class="resultado_pesquisa" >
+            <img class="img img-polaroid img-rounded" data-src="holder.js/225x135/auto"/>
+            <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+            <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+            <div>
+                <input type="button" class="btn btn-warning btn-mini"  value="Ver detalhe [+]"/>
+                <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
             </div>
         </div>
-   
+        
+    </div>
+
+
 </div>
 
 <div class="pagination" id="paginacao" align="center">
-        <ul>
-          <li><a href="#"><<</a></li>
-          <li><a href="#">1</a></li>
-          <li><a href="#">2</a></li>
-          <li><a href="#">3</a></li>
-          <li><a href="#">4</a></li>
-          <li><a href="#">5</a></li>
-          <li><a href="#">>></a></li>
-        </ul>
-    </div>
+    <ul>
+        <li><a href="#"><<</a></li>
+        <li><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">5</a></li>
+        <li><a href="#">>></a></li>
+    </ul>
+</div>
