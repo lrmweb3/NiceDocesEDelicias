57eb6902b0e9fd066e136e1df5c32a2fae42c835
MovimentoWeb <reginaldo963@gmail.com>
2015-05-25 14:56:23 -0300

    Revert "Construindo página Lançamentos"
    This reverts commit 9d314f429b50f5790dd66bb61ead3d6c51c6b7f6.

--- application/views/site/pg/lancamentos/lancamentos_view.php
+++ application/views/site/pg/lancamentos/lancamentos_view.php
@@ -1,19 +1,3 @@
 <div class="container">
-    <div class="row-fluid">
-        <div class="span4">
-            <h1 class="label label-info tituloChamada">Artigos relacionados</h1>
-            <img class="img img-polaroid img-rounded" data-src="holder.js/360x216/auto"/>
-        </div>
-        <div class="span4 well">
-            <div class="row-fluid">
-                <div class="span8"></div>
-                <div class="span4"><img class="img img-polaroid img-rounded" data-src="holder.js/350x210/auto"/></div>
-            </div>
-        </div>
-        <div class="span4">
-            <img class="img img-polaroid img-rounded" data-src="holder.js/360x216/auto"/>
-        </div>
-    </div>
-    <div class="row-fluid"></div>
-    <div class="row-fluid"></div>
+    <h1>Lançamentos</h1>
 </div>
