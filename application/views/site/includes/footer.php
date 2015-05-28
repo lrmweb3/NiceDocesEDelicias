a38793ba799c5730bc6371297c99c233903ac8c6
Araujo <Araujo@Douglas>
2015-05-28 14:12:09 -0300

    edição do footer 2(incompleto)

--- application/views/site/includes/footer.php
+++ application/views/site/includes/footer.php
@@ -1,17 +1,28 @@
 <footer class="footer">
     <div class="container">
         <div class="span4 input-large search-query fd_mapa_site" >
-            1
+
+
+            <dl>
+                <dt><u><h4> Mapa do Site</h4></u></dt>
+                <dd>Lançamentos</dd>
+                <dd>Doce</dd>
+                <dd>Salgados</dd>
+                <dd>Serviços</dd>
+                <dd>Eventos</dd>
+                <dd>Contato</dd>
+                <dd>Entrar com sua conta(Login)</dd>
+            </dl>
         </div>
-        
+
         <div class="span4 input-large search-query fd_parceiro_colaboradores" >
-            2
+            <u><h4>Parceiros e Colaboradores</h4></u>
         </div>
-        
+
         <div class="span4 input-large search-query fd_redes_sociais" >
-            3
+            <u><h4>Redes Sociais</h4></u>
         </div>
-            
+
     </div>     
 </footer>
 
