eb55c916dd6fd2ac2572f0e9e0aee8773f69d517
MovimentoWeb <reginaldo963@gmail.com>
2015-05-31 11:17:34 -0300

    Slide show -  página Lançamentos

--- application/views/site/pg/lancamentos/pg/lista.php
+++ application/views/site/pg/lancamentos/pg/lista.php
@@ -2,6 +2,38 @@
 <div class="page-header">
     <h1>Lançamentos</h1>
 </div>
+<div class="row-fluid">
+    <div id="myCarousel" class="carousel slide">
+        <!-- Itens de carousel -->
+        <div class="carousel-inner">
+            <div class="active item">
+                <a href="">
+                    <img src="<?php echo base_url('img/headerSlideShow/1.jpg') ?>" alt="">
+                </a>
+                <div class="carousel-caption">
+                    <h4>Primeiro rótulo de miniatura</h4>
+                    <div class="visible-desktop hidden-phone hidden-tablet">
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <a href="">
+                    <img src="<?php echo base_url('img/headerSlideShow/3.jpg') ?>" alt="">
+                </a>
+                <div class="carousel-caption">
+                    <h4>Terceiro rótulo de miniatura</h4>
+                    <div class="visible-desktop hidden-phone hidden-tablet">
+                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Navegador do carousel -->
+        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+    </div>
+</div>
 <div class="result-group">
     <div class="resultado_lancamento" >
         <a href="<?php echo base_url('lancamentos/item') ?>"><img class="img img-polaroid img-rounded" data-src="holder.js/360x216/auto"/></a>
@@ -30,16 +62,24 @@
             <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
         </div>
     </div>
+    <div class="resultado_lancamento" >
+        <a href="<?php echo base_url('lancamentos/item') ?>"><img class="img img-polaroid img-rounded" data-src="holder.js/360x216/auto"/></a>
+        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+        <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+        <div>
+            <a href="<?php echo base_url('lancamentos/item') ?>" class="btn btn-warning btn-mini">Ver detalhe [+]</a>
+            <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
+        </div>
+    </div>
+    <div class="resultado_lancamento" >
+        <a href="<?php echo base_url('lancamentos/item') ?>"><img class="img img-polaroid img-rounded" data-src="holder.js/360x216/auto"/></a>
+        <h5 id="titulo_pesquisa">Titulo da Imagem</h5>
+        <span>Conteudo referente a imagem mostrada acima. Especificações do produto em questão.</span>    
+        <div>
+            <a href="<?php echo base_url('lancamentos/item') ?>" class="btn btn-warning btn-mini">Ver detalhe [+]</a>
+            <input type="button" class="btn btn-success btn-mini" value="Indicar para um amigo >>" />
+        </div>
+    </div>
+   
 </div>
 
-<div class="pagination" id="paginacao" align="center">
-    <ul>
-        <li><a href="#"><<</a></li>
-        <li><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
-        <li><a href="#">>></a></li>
-    </ul>
-</div>
