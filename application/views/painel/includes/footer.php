7e924112e6ba9ba78085c417e4b71752df5d9754
Araujo <Araujo@Douglas>
2015-06-07 15:46:43 -0300

    redes sociais (rodapé)

--- application/views/painel/includes/footer.php
+++ application/views/painel/includes/footer.php
@@ -18,9 +18,16 @@
                 <u><h4>Parceiros e Colaboradores</h4></u>
             </div>
             <div class="span3">
-                <u><h4>Redes Sociais</h4></u>
+                <lt> <u><h4>Redes Sociais</h4></u></lt>
+                <dd><img src="<?php echo base_url("img/redesSociais/facebook_mini.png") ?>" alt="Página no Facebook" title="Curta nossa página no Facebook" /><a href=""> /Facebook</a></dd>
+                <dd><img src="<?php echo base_url("img/redesSociais/twitter_mini.png") ?>" alt="Siga-no no Twitter" title="Siga-no no Twiter" /><a href=""> @Twitter</a></dd>
+                <dd><img src="<?php echo base_url("img/redesSociais/youtube_mini.png") ?>" alt="Veja nossos vídeos" title="Veja nossos vídeos no Youtube" /><a href=""> /Youtube</a></dd>
+                <dd><img src="<?php echo base_url("img/redesSociais/skype_mini.png") ?>" alt="Converse conosco agora pelo Skype" title="Converse conosco agora pelo Skype" /><a href=""> Skype</a></dd>
+                <dd><img src="<?php echo base_url("img/redesSociais/google+_mini.png") ?>" alt="Entre para nosso Círculo no Google+" title="Entre pra nosso Círculo de comunidade no Google+" /><a href=""> /Google+</a></dd>
+                <dd><img src="<?php echo base_url("img/redesSociais/linkedin_mini.png") ?>" alt="Conecte-se ao nosso LinkedIn" title="Conecte-se ao nosso Linkedin" /><a href=""> /Linkedin</a></dd>
+                <dd><img src="<?php echo base_url("img/redesSociais/adm_mini.png") ?>" alt="Painel Administrativo" title="Painel Administrativo" /> <a href="<?php echo base_url('painel'); ?>"> Painel Administrativo</a></dd>
+                
             </div>
         </div>
     </div>
 </footer>
-
